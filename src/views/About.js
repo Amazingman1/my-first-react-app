8da75bd614504b3b02dca217acffbc77f599b297
Amazingman1 <wangxuanniu@yeah.net>
2020-08-20 06:33:12 +0800

    chore:新增react-router-dom

---
+++ src/views/About.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+class About extends Component{
+  constructor(porps){
+    super(porps)
+    this.state={}
+  }
+  render(){
+    return <div>About</div>
+  }
+}
+export default About
