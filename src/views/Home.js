0790887d28b0236b7340f9ff6c90b5cbe89885ed
Amazingman1 <wangxuanniu@yeah.net>
2020-08-22 11:44:29 +0800

    feate:安装ant 按需引入

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,12 +1,19 @@
-import React, {Component} from 'react'
+import React, {Component, Fragment} from 'react'
 import './home.scss'
+import { Button } from 'antd'
+
 class Home extends Component{
   constructor(porps){
     super(porps)
     this.state={}
   }
   render(){
-    return <div>Home</div>
+    return (
+      <Fragment>
+        home
+        <Button type="primary">按 钮</Button>
+      </Fragment>
+    )
   }
 }
 export default Home
